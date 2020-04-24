8b10e08c58a70c82f323da8c8751cbc8dda20245
JunfengHan <505065510@qq.com>
2020-04-25 00:09:38 +0800

    修改配置文件，修改 git 用户信息

--- src/config/config.module.ts
+++ src/config/config.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { EnvService } from './env.service';
 import { join } from 'path';
+import { BussConf } from './buss-conf';
 
 const ENV_PATH = process.env.CONF_PATH || join(__dirname, '../../config/');
 process.env.CONF_PATH = ENV_PATH;
@@ -12,8 +13,9 @@ const envFile = ENV_PATH + (process.env.CONF_FILE || 'data.env');
       provide: EnvService,
       useValue: new EnvService(envFile),
     },
+    BussConf,
   ],
-  exports: [EnvService]
+  exports: [EnvService, BussConf,]
 })
 
 export class ConfigModule {}
